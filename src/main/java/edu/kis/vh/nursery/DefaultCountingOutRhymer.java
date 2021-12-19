7fbdbf057d4c37abcaee8952f05f880c1cd77b16
Damian <pk-14@sr.dmcs.pl>
2021-12-19 21:10:56 +0100

    1.4 stale 1 FIX

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -15,22 +15,22 @@ public class DefaultCountingOutRhymer {
 	}
 
 	public boolean callCheck() {
-		return total == -1;
+		return total == EMPTY_STACK_VALUE;
 	}
 
 	public boolean isFull() {
-		return total == 11;
+		return total == LAST_VALUE;
 	}
 
 	protected int peekaboo() {
 		if (callCheck())
-			return -1;
+			return EMPTY_STACK_VALUE;
 		return numbers[total];
 	}
 
 	public int countOut() {
 		if (callCheck())
-			return -1;
+			return EMPTY_STACK_VALUE;
 		return numbers[total--];
 	}
 

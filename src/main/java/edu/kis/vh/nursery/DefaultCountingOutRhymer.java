cb36c9ed6f2a4b689a28b644b09feca16be2f371
RogiGral <igor.gralak99@gmail.com>
2021-12-19 21:41:13 +0100

    1.4 accesory 4

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -8,13 +8,6 @@ public class DefaultCountingOutRhymer {
 
 	private final int[] numbers = new int[STACK_CAPACITY];
 
-	public int getTotal() {
-		return total;
-	}
-
-	public void setTotal(int total) {
-		this.total = total;
-	}
 
 	private int total = EMPTY_STACK_VALUE;
 
